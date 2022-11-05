57e38b021b335e659d895b81edcfeca3623c8a2f
beiduoxiao <beiduo.xiao@gmail.com>
2022-11-05 00:40:11 -0500

    The POC to test the system design - implemented receiving messages

--- app/src/main/java/edu/northeastern/team36/A8MainActivity.java
+++ app/src/main/java/edu/northeastern/team36/A8MainActivity.java
@@ -3,14 +3,20 @@ package edu.northeastern.team36;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.security.Timestamp;
 import java.text.SimpleDateFormat;
@@ -18,6 +24,8 @@ import java.util.Date;
 import java.util.UUID;
 
 public class A8MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "A8MainActivity";
 
     private DatabaseReference myDB;
     private TextView helloCurrentUser;
@@ -28,7 +36,6 @@ public class A8MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnSend;
     private TextView txtReceived;
-    private TextView txtSent;
 
 
 
@@ -45,7 +52,6 @@ public class A8MainActivity extends AppCompatActivity {
         btnLogin = (Button) findViewById(R.id.button3);
         btnSend = (Button) findViewById(R.id.button4);
         txtReceived = (TextView) findViewById(R.id.textView4);
-        txtSent = (TextView) findViewById(R.id.textView5);
 
 
 
@@ -70,14 +76,49 @@ public class A8MainActivity extends AppCompatActivity {
         // Connect with firebase
         myDB = FirebaseDatabase.getInstance().getReference();
 
-        // Insert 1 message object into DB
+        // Add DB updates listener
+        myDB.child("messages").addChildEventListener(
+                new ChildEventListener() {
 
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        showMessage(dataSnapshot);
+                        Log.e(TAG, "onChildAdded: dataSnapshot = " + dataSnapshot.getValue().toString());
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        showMessage(dataSnapshot);
+                        Log.v(TAG, "onChildChanged: " + dataSnapshot.getValue().toString());
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e(TAG, "onCancelled:" + databaseError);
+                        Toast.makeText(getApplicationContext()
+                                , "DBError: " + databaseError, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+
+        // Send message - insert 1 message object into DB
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 if(currentUser == "NONE"){
-                    // Ask to login
+                    Toast.makeText(getApplicationContext(), "Please login at first!", Toast.LENGTH_LONG).show();
                 } else {
                     String messageID = "message_" + UUID.randomUUID().toString();
                     String fromUsername = currentUser;
@@ -96,9 +137,20 @@ public class A8MainActivity extends AppCompatActivity {
 
     }
 
+
     public void writeNewMessage(String messageID, String fromUsername, String toUsername, String timestamp, String stickerID) {
         RTDBMessage message = new RTDBMessage(fromUsername, toUsername, timestamp, stickerID);
 
         myDB.child("messages").child(messageID).setValue(message);
+    }
+
+    private void showMessage(DataSnapshot dataSnapshot) {
+        RTDBMessage message = dataSnapshot.getValue(RTDBMessage.class);
+        if (message.toUsername.equals(currentUser)){
+            txtReceived.append("\n\nfrom: " + message.fromUsername + "\nstickerID: " + message.stickerID + "\ntime: " + message.timestamp);
+        }
+
+
+
     }
 }
