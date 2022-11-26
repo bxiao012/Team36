4b89f557307818cf3d59c7f2fe157618334f54f5
beebee1 <dai.linh@northeastern.edu>
2022-11-26 14:17:18 -0800

    create profile activity

---
+++ app/src/main/java/edu/northeastern/team36/FinalProject/Review.java
@@ -0,0 +1,16 @@
+package edu.northeastern.team36.FinalProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.team36.R;
+
+public class Review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_review);
+    }
+}
