b3b43459a6c8be6e73b32d2a5b618feaf2c27289
Lu Wang <wang.lu6@northeastern.edu>
2022-12-01 21:36:34 -0800

    add navigation to activities

--- app/src/main/java/edu/northeastern/team36/FinalProject/FinalProjectActivity.java
+++ app/src/main/java/edu/northeastern/team36/FinalProject/FinalProjectActivity.java
@@ -1,14 +1,27 @@
 package edu.northeastern.team36.FinalProject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+import edu.northeastern.team36.MainActivity;
 import edu.northeastern.team36.R;
 
 public class FinalProjectActivity extends AppCompatActivity {
@@ -36,9 +49,36 @@ public class FinalProjectActivity extends AppCompatActivity {
         postAdapter = new PostAdapter(postArrayList, username, this);
         postRecyclerView.setAdapter(postAdapter);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_LABELED);
 
+        bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    Intent intent0 = new Intent(FinalProjectActivity.this, FinalProjectActivity.class);
+                    startActivity(intent0);
+                    break;
+                case R.id.nav_reviews:
+                    Intent intent1 = new Intent(FinalProjectActivity.this, ReviewsActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.nav_profile:
+                    Intent intent2 = new Intent(FinalProjectActivity.this,ProfileActivity.class);
+                    startActivity(intent2);
+                    break;
+            }
+
+            return true;
+        });
+
+        }
+
+
+    @SuppressLint("RestrictedApi")
+    public static void removeNavigationShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        menuView.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_LABELED);
+        menuView.buildMenuView();
+    }
     }
 
-
-
-}
