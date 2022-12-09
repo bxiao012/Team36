31639995863844bd099667982d3f8d3be40bbfde
beebee1 <dai.linh@northeastern.edu>
2022-12-09 10:51:50 -0800

    convert bitmap to string and store the string into database

--- app/src/main/java/edu/northeastern/team36/FinalProject/CreatePostActivity.java
+++ app/src/main/java/edu/northeastern/team36/FinalProject/CreatePostActivity.java
@@ -24,6 +24,7 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +47,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import java.io.ByteArrayOutputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -54,7 +56,6 @@ import edu.northeastern.team36.FinalProject.DAO.MyRunnable;
 import edu.northeastern.team36.R;
 
 public class CreatePostActivity extends AppCompatActivity {
-    private static final int REQUEST_IMAGE_CAPTURE = 101;
     private static final int LOCATION_REQUEST_CODE = 10001;
     private static final int CAMERA_REQUEST = 1888;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
@@ -70,6 +71,7 @@ public class CreatePostActivity extends AppCompatActivity {
     private String location;
     private String username;
     private String userID;
+    private String photo;
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
     ImageView imageV;
@@ -96,7 +98,9 @@ public class CreatePostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
         username = getIntent().getStringExtra("username");
-        userID = getIntent().getStringExtra("username");
+        userID = getIntent().getStringExtra("userID");
+        // default photo
+        photo = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMAAAACoCAYAAAC2e+";
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(CreatePostActivity.this);
         // location request
@@ -117,11 +121,14 @@ public class CreatePostActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         // Add same code that you want to add in onActivityResult method
-                        if(result.getResultCode() == CAMERA_REQUEST) {
+                        if(result.getResultCode() == RESULT_OK && result.getData() !=null) {
                             Intent intent = result.getData();
-                            Bundle im = intent.getExtras();
-                            Bitmap imBitmap = (Bitmap) im.get("camera result");
-                            imageV.setImageBitmap(imBitmap);
+                            Bundle picture = intent.getExtras();
+                            Bitmap pictureBitmap = (Bitmap) picture.get("data");
+                            imageV.setImageBitmap(pictureBitmap);
+                            // encode the bitmap
+                            photo = getEncodedString(pictureBitmap);
+                            System.out.println("photo: " + photo);
                         }
                     }
                 });
@@ -177,14 +184,15 @@ public class CreatePostActivity extends AppCompatActivity {
                 post.addProperty("createTime", formatDate);
                 JsonObject owner = new JsonObject();
                 JsonObject ownerID = new JsonObject();
-                ownerID.addProperty("$oid", "637ce04eb5eb013ea20e7010");
-                owner.addProperty("name","user10");
+                ownerID.addProperty("$oid", userID);
+                owner.addProperty("name",username);
                 owner.add("id", ownerID);
                 post.add("owner", owner);
                 post.add("applied", new JsonArray());
                 post.add("selected", new JsonArray());
                 JsonObject imageObj = new JsonObject();
-                imageObj.addProperty("img","data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMAAAACoCAYAAAC2e+");
+                imageObj.addProperty("img",photo);
+                System.out.println("upload photo: " + "data:image/png;base64," + photo);
                 imageObj.addProperty("uploadTime", formatDate);
                 MyRunnable handleMessage = new MyRunnable() {
                     JsonObject message;
@@ -283,13 +291,14 @@ public class CreatePostActivity extends AppCompatActivity {
         }
     }
 
-    // take picture
-    public void takePicture(View view) {
-        Intent image = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    // encode bitmap into string-base_64
+    private String getEncodedString(Bitmap bitmap){
 
-        if(image.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(image, REQUEST_IMAGE_CAPTURE);
-        }
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, os);
+        byte[] imageArr = os.toByteArray();
+
+        return Base64.encodeToString(imageArr, Base64.DEFAULT);
     }
-
 }
