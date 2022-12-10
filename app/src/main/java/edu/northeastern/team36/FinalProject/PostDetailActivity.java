3c63f677104853b0aac9dd02529e8d600d043587
beebee1 <dai.linh@northeastern.edu>
2022-12-09 22:38:22 -0800

    connect PostDetailActivity to ProfileActivity

--- app/src/main/java/edu/northeastern/team36/FinalProject/PostDetailActivity.java
+++ app/src/main/java/edu/northeastern/team36/FinalProject/PostDetailActivity.java
@@ -8,14 +8,20 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,10 +33,18 @@ import edu.northeastern.team36.R;
 public class PostDetailActivity extends AppCompatActivity {
     private static final String TAG = "PostDetailActivity";
     private String postID, username, userID;
+    private String passUsername, passUserID;
     private String title, authorName, description, seatRemaining, gameName, gameTime, status, createTime;
     private List<Map> appliedUsers, selectedUsers;
-    private TextView titleTv, authorTv, descriptionTv, seatTv, gameTv;
+    private TextView titleTv, authorTv, descriptionTv, seatTv, gameTv, gameTimeTv;
     private ImageView imageView;
+    Spinner appliedSpinner;
+    Spinner selectedSpinner;
+    Button enterApplied;
+    Button enterSelected;
+    Button enterOwner;
+    Map<String, String> appliedMap = new HashMap<>();
+    Map<String, String> selectedMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +61,59 @@ public class PostDetailActivity extends AppCompatActivity {
         descriptionTv = (TextView) findViewById(R.id.descriptionTv2);
         seatTv = (TextView) findViewById(R.id.seatTv2);
         gameTv = (TextView) findViewById(R.id.gameTv2);
+        gameTimeTv = (TextView) findViewById(R.id.game_time_tv);
         imageView = (ImageView) findViewById(R.id.imageTv2);
+        appliedSpinner = (Spinner) findViewById(R.id.applied_spinner);
+        selectedSpinner = (Spinner) findViewById(R.id.selected_spinner);
+        enterApplied = (Button) findViewById(R.id.applied_button);
+        enterSelected = (Button) findViewById(R.id.selected_button);
+        enterOwner = (Button) findViewById(R.id.enter_owner);
 
         // get postDetail data from database
         findPostWithImage();
+
+        // enter profile page
+        enterApplied.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(appliedSpinner.getSelectedItem() == null) {
+                    Toast.makeText(PostDetailActivity.this, "No applied user selected!", Toast.LENGTH_SHORT).show();
+                } else {
+                    passUsername = appliedSpinner.getSelectedItem().toString();
+                    passUserID = appliedMap.get(passUsername);
+                    Intent intent = new Intent(PostDetailActivity.this,ProfileActivity.class);
+                    intent.putExtra("username", passUsername);
+                    intent.putExtra("userID", passUserID);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        enterSelected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedSpinner.getSelectedItem() == null) {
+                    Toast.makeText(PostDetailActivity.this, "No selected user selected!", Toast.LENGTH_SHORT).show();
+                } else {
+                    passUsername = selectedSpinner.getSelectedItem().toString();
+                    passUserID = selectedMap.get(passUsername);
+                    Intent intent = new Intent(PostDetailActivity.this,ProfileActivity.class);
+                    intent.putExtra("username", passUsername);
+                    intent.putExtra("userID", passUserID);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        enterOwner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(PostDetailActivity.this,ProfileActivity.class);
+                    intent.putExtra("username", passUsername);
+                    intent.putExtra("userID", passUserID);
+                    startActivity(intent);
+            }
+        });
 
     }
 
@@ -85,17 +148,52 @@ public class PostDetailActivity extends AppCompatActivity {
                     appliedUsers = (List<Map>) postMap.get("applied");
                     selectedUsers = (List<Map>) postMap.get("selected");
 
+                    List<String> appliedUsersList = new ArrayList<>();
+                    List<String> selectedUsersList = new ArrayList<>();
+
+                    // set applied users spinner
+                    if (!appliedUsers.isEmpty()) {
+                        for(Map appliedUser : appliedUsers) {
+                            appliedUsersList.add(appliedUser.get("name").toString());
+                            appliedMap.put(appliedUser.get("name").toString(), appliedUser.get("id").toString());
+                        }
+                        String[] appliedArray = new String[appliedUsersList.size()];
+                        appliedArray = appliedUsersList.toArray(appliedArray);
+
+                        ArrayAdapter<String> appliedAdapter = new ArrayAdapter<String>
+                                (PostDetailActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,appliedArray);
+                        appliedSpinner.setAdapter(appliedAdapter);
+                    }
+
+
+                    // set selected users spinner
+                    if (!selectedUsers.isEmpty()) {
+                        for(Map selectedUser : selectedUsers) {
+                            selectedUsersList.add(selectedUser.get("name").toString());
+                            selectedMap.put(selectedUser.get("name").toString(), selectedUser.get("id").toString());
+                        }
+
+                        String[] selectedArray = new String[appliedUsersList.size()];
+                        selectedArray = appliedUsersList.toArray(selectedArray);
+
+                        ArrayAdapter<String> selectedAdapter = new ArrayAdapter<String>
+                                (PostDetailActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,selectedArray);
+                        selectedSpinner.setAdapter(selectedAdapter);
+                    }
+
 //                    Log.e(TAG, " applied: " + appliedUsers.toString() + " selected: " + selectedUsers.toString());
 
                     // get other useful data
                     Map ownerMap = (Map) postMap.get("owner");
+                    passUserID = ownerMap.get("id").toString();
+                    passUsername = ownerMap.get("name").toString();
                     title = postMap.get("title").toString();
                     authorName = ownerMap.get("name").toString();
                     description = postMap.get("content").toString();
                     gameName = postMap.get("gameName").toString();
-                    gameTime = postMap.get("gameTime").toString();
                     status = postMap.get("status").toString();
                     createTime = postMap.get("createTime").toString();
+                    gameTime = postMap.get("gameTime").toString();
                     Double doubleSeat = (Double) postMap.get("seat");
                     seatRemaining = "Seats: " + selectedUsers.size() + "/" + doubleSeat.intValue();
 
@@ -105,6 +203,7 @@ public class PostDetailActivity extends AppCompatActivity {
                     descriptionTv.setText(description);
                     seatTv.setText(seatRemaining);
                     gameTv.setText(gameName);
+                    gameTimeTv.setText(gameTime);
 
                     // process image and set image to xml
                     List<Map> imgArray = (List<Map>) postMap.get("imageDetail");
